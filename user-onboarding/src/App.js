cce63dcc4da7c0baa36589d17f598da4d64ee168
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-04-22 21:02:03 -0700

    attempted stretch

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import Form from './components/form.component'
+import './App.css';
+
+function App() {
+  return (
+        <div>
+
+          <Form />
+
+        </div>
+      );
+    }
+    
+    export default App;
